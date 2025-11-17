68cc4cba3adab3e263a4d84fc43dababeeea8275
Ammar Rajab <ammar.earth@gmail.com>
2025-11-17 19:31:15 +0400

    Initial commit

---
+++ RealTimePriceTrackerTests/RealTimePriceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimePriceTrackerTests.swift
+//  RealTimePriceTrackerTests
+//
+//  Created by Ammar on 17/11/2025.
+//
+
+import Testing
+@testable import RealTimePriceTracker
+
+struct RealTimePriceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
